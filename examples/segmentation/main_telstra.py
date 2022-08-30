a8b7644bcaf97dae77a4f223254ac97f9e465749
junaid-maqbool <junmaq34@gmail.com>
2022-08-30 13:59:07 +0500

    update main_telstra.py

--- examples/segmentation/main_telstra.py
+++ examples/segmentation/main_telstra.py
@@ -11,17 +11,15 @@ from tqdm import tqdm
 import torch, torch.nn as nn
 from torch import distributed as dist, multiprocessing as mp
 from torch.utils.tensorboard import SummaryWriter
-from torch_scatter import scatter
 from openpoints.utils import set_random_seed, save_checkpoint, load_checkpoint, resume_checkpoint, setup_logger_dist, \
     cal_model_parm_nums, Wandb, generate_exp_directory, resume_exp_directory, EasyConfig, dist_utils, find_free_port
 from openpoints.utils import AverageMeter, ConfusionMatrix, get_mious
 from openpoints.dataset import build_dataloader_from_cfg, get_scene_seg_features, get_class_weights
-from openpoints.dataset.data_util import voxelize
-from openpoints.transforms import build_transforms_from_cfg
 from openpoints.optim import build_optimizer_from_cfg
 from openpoints.scheduler import build_scheduler_from_cfg
 from openpoints.loss import build_criterion_from_cfg
 from openpoints.models import build_model_from_cfg
+from openpoints.dataset.vis3d import write_obj
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -109,14 +107,10 @@ def main(gpu, cfg):
                 return val_miou
             elif cfg.mode == 'test':
                 best_epoch, best_val = load_checkpoint(model, pretrained_path=cfg.pretrained_path)
-                miou, macc, oa, ious, accs, _ = test_entire_room(model, cfg.dataset.common.test_area, cfg)
-                with np.printoptions(precision=2, suppress=True):
-                    logging.info(f'Best ckpt @E{best_epoch},  test_oa , test_macc, test_miou: {oa:.2f} {macc:.2f} '
-                                 f'{miou:.2f}, , ious {ious}, accs: {accs}'
-                                 f'\niou per cls is: {ious}')
-                cfg.csv_path = os.path.join(cfg.run_dir, cfg.run_name + '_test.csv')
-                write_to_csv(oa, macc, miou, ious, best_epoch, cfg)
-                return miou 
+                test_miou, test_macc, test_oa, test_ious, test_accs = test(model, val_loader, cfg)
+                print(f'test miou: {test_miou:.3f}, test macc: {test_macc}, test oa: {test_oa}, '
+                      f'\ntest_ious: {test_ious}, test accs: {test_accs}')
+                return None
             else:
                 logging.info(f'Finetuning from {cfg.pretrained_path}')
                 model.load_model_from_ckpt(cfg.pretrained_path, only_encoder=cfg.only_encoder)
@@ -153,7 +147,7 @@ def main(gpu, cfg):
 
         is_best = False
         if epoch % cfg.val_freq == 0:
-            val_miou, val_macc, val_oa, val_ious, val_accs = validate_fn(model, val_loader, cfg)
+            val_miou, val_macc, val_oa, val_ious, val_accs = validate_fn(model, train_loader, cfg)
             if val_miou > best_val:
                 is_best = True
                 best_val = val_miou
@@ -165,6 +159,8 @@ def main(gpu, cfg):
                     logging.info(
                         f'Find a better ckpt @E{epoch}, val_miou {val_miou:.2f} val_macc {macc_when_best:.2f}, val_oa {oa_when_best:.2f}'
                         f'\nmious: {val_ious}, val_ious {val_ious}, val_accs: {val_accs}')
+                test(model, val_loader, cfg)
+
 
         lr = optimizer.param_groups[0]['lr']
         logging.info(f'Epoch {epoch} LR {lr:.6f} '
@@ -190,11 +186,6 @@ def main(gpu, cfg):
                             is_best=is_best
                             )
             is_best = False
-    # do not save file to wandb to save wandb space
-    # if writer is not None:
-    #     Wandb.add_file(os.path.join(cfg.ckpt_dir, f'{cfg.run_name}_ckpt_best.pth'))
-    # Wandb.add_file(os.path.join(cfg.ckpt_dir, f'{cfg.logname}_ckpt_latest.pth'))
-
     # validate
     with np.printoptions(precision=2, suppress=True):
         logging.info(
@@ -203,11 +194,8 @@ def main(gpu, cfg):
     
     # test
     load_checkpoint(model, pretrained_path=os.path.join(cfg.ckpt_dir, f'{cfg.run_name}_ckpt_best.pth'))
-    cfg.csv_path = os.path.join(cfg.run_dir, cfg.run_name + f'_area5.csv')
-    if 'sphere' in cfg.dataset.common.NAME.lower():
-        test_miou, test_macc, test_oa, test_ious, test_accs = validate_sphere(model, val_loader, cfg)
-    else:
-        test_miou, test_macc, test_oa, test_ious, test_accs, _  = test_entire_room(model, cfg.dataset.common.test_area, cfg)
+    cfg.csv_path = os.path.join(cfg.run_dir, cfg.run_name + f'_area_{2}.csv')
+    test_miou, test_macc, test_oa, test_ious, test_accs = test(model, val_loader, cfg)
     with np.printoptions(precision=2, suppress=True):
         logging.info(f'Best ckpt @E{best_epoch},  test_oa {test_oa:.2f}, test_macc {test_macc:.2f}, test_miou {test_miou:.2f}, '
                     f'\niou per cls is: {test_ious}')
@@ -217,20 +205,6 @@ def main(gpu, cfg):
         writer.add_scalar('test_oa', test_oa, epoch)
     write_to_csv(test_oa, test_macc, test_miou, test_ious, best_epoch, cfg, write_header=True)
     logging.info(f'save results in {cfg.csv_path}')
-    if cfg.use_voting:
-        load_checkpoint(model, pretrained_path=os.path.join(cfg.ckpt_dir, f'{cfg.run_name}_ckpt_best.pth'))
-        set_random_seed(cfg.seed)
-        val_miou, val_macc, val_oa, val_ious, val_accs = validate_fn(model, val_loader, cfg, num_votes=20,
-                                                                     data_transform=data_transform)
-        if writer is not None:
-            writer.add_scalar('val_miou20', val_miou, cfg.epochs + 50)
-
-        ious_table = [f'{item:.2f}' for item in val_ious]
-        data = [cfg.cfg_basename, 'True', f'{val_oa:.2f}', f'{val_macc:.2f}', f'{val_miou:.2f}'] + ious_table + [
-            str(best_epoch), cfg.run_dir]
-        with open(cfg.csv_path, 'w', encoding='UT8') as f:
-            writer = csv.writer(f)
-            writer.writerow(data)
     return True
 
 
@@ -246,10 +220,6 @@ def train_one_epoch(model, train_loader, criterion, optimizer, scheduler, epoch,
             data[key] = data[key].cuda(non_blocking=True)
         num_iter += 1
         target = data['y'].squeeze(-1)
-        # # debug
-        # from openpoints.dataset import vis_points
-        # vis_points(data['pos'].cpu().numpy()[0], labels=data['y'].cpu().numpy()[0])
-        # vis_points(data['pos'].cpu().numpy()[0], data['x'][0, :3, :].transpose(1, 0))
         if len(data['x'].shape) > 2:
             data['x'] = data['x'].transpose(1, 2)
         data['x'] = get_scene_seg_features(cfg.model.in_channels, data['pos'], data['x'])
@@ -281,6 +251,9 @@ def train_one_epoch(model, train_loader, criterion, optimizer, scheduler, epoch,
 
 @torch.no_grad()
 def validate(model, val_loader, cfg, num_votes=1, data_transform=None):
+    cfg.val_vis_dir = os.path.join(cfg.run_dir, 'visualization_val')
+    os.makedirs(cfg.val_vis_dir, exist_ok=True)
+    cfg.cmap = cfg.cmap.astype(np.float32)
     model.eval()  # set model to eval mode
     cm = ConfusionMatrix(num_classes=cfg.num_classes, ignore_index=cfg.ignore_index)
     pbar = tqdm(enumerate(val_loader), total=val_loader.__len__())
@@ -292,184 +265,50 @@ def validate(model, val_loader, cfg, num_votes=1, data_transform=None):
         if len(data['x'].shape) > 2:
             data['x'] = data['x'].transpose(1, 2)
         data['x'] = get_scene_seg_features(cfg.model.in_channels, data['pos'], data['x'])
-
         logits = model(data)
-        # if 'mask' not in cfg.criterion.NAME or cfg.get('use_maks', False):
         cm.update(logits.argmax(dim=1), target)
-        # else:
-        #     mask = data['mask'].bool()
-        #     cm.update(logits.argmax(dim=1)[mask], target[ma])
+        pred = logits.argmax(dim=1).cpu().numpy().squeeze()
+        pred = cfg.cmap[pred, :]
+        pos = data['pos'].detach().cpu().numpy()
+        write_obj(pos[0], pred, os.path.join(cfg.val_vis_dir, f'Area_{2}_{idx}.obj'))
+
     tp, union, count = cm.tp, cm.union, cm.count
-    # print(f'before gathering, rank {cfg.rank} \n tp {tp} \n union {union} \n count {count}')
     if cfg.distributed:
         dist.all_reduce(tp), dist.all_reduce(union), dist.all_reduce(count)
-    # print(f'after gathering, rank {cfg.rank} \n union {union} \n count {count}')
     miou, macc, oa, ious, accs = get_mious(tp, union, count)
     return miou, macc, oa, ious, accs
 
 
 @torch.no_grad()
-def validate_sphere(model, val_loader, cfg, num_votes=1, data_transform=None):
-    """
-    validation for sphere sampled input points with mask.
-    in this case, between different batches, there are overlapped points.
-    thus, one point can be evaluated multiple times.
-    In this validate_mask, we will avg the logits.
-    """
+def test(model, val_loader, cfg, num_votes=1, data_transform=None):
+    from openpoints.dataset.vis3d import write_obj
+    cfg.vis_dir = os.path.join(cfg.run_dir, 'visualization_test')
+    os.makedirs(cfg.vis_dir, exist_ok=True)
+    cfg.cmap = cfg.cmap.astype(np.float32)
+
     model.eval()  # set model to eval mode
     cm = ConfusionMatrix(num_classes=cfg.num_classes, ignore_index=cfg.ignore_index)
     pbar = tqdm(enumerate(val_loader), total=val_loader.__len__())
-    all_logits, all_point_inds = [], []
     for idx, data in pbar:
-        for key in data.keys():
+        keys = data.keys() if callable(data.keys) else data.keys
+        for key in keys:
             data[key] = data[key].cuda(non_blocking=True)
+        target = data['y'].squeeze(-1)
         if len(data['x'].shape) > 2:
             data['x'] = data['x'].transpose(1, 2)
         data['x'] = get_scene_seg_features(cfg.model.in_channels, data['pos'], data['x'])
         logits = model(data)
-        all_logits.append(logits)
-        all_point_inds.append(data['input_inds'])
+        cm.update(logits.argmax(dim=1), target)
+        pred = logits.argmax(dim=1).cpu().numpy().squeeze()
+        pred = cfg.cmap[pred, :]
+        pos = data['pos'].detach().cpu().numpy()
+        write_obj(pos[0], pred, os.path.join(cfg.vis_dir, f'pred-Area{2}-{idx}.obj'))
 
-    all_logits = torch.cat(all_logits, dim=0).transpose(1, 2).reshape(-1, cfg.num_classes)
-    all_point_inds = torch.cat(all_point_inds, dim=0).flatten()
-
-    if cfg.distributed:
-        dist.all_reduce(all_logits), dist.all_reduce(all_point_inds)
-
-    # subsampled points: 5323316 points.
-    all_logits = scatter(all_logits, all_point_inds, dim=0, reduce='mean')
-
-    # now, project the original points to the subsampled points
-    # these two targets would be very similar but not the same
-    # val_points_targets = all_targets[val_points_projections]
-    # torch.allclose(val_points_labels, val_points_targets)
-    all_logits = all_logits.argmax(dim=1)
-    val_points_labels = torch.from_numpy(val_loader.dataset.clouds_points_labels[0]).squeeze(-1).to(all_logits.device)
-    val_points_projections = torch.from_numpy(val_loader.dataset.projections[0]).to(all_logits.device).long()
-    val_points_preds = all_logits[val_points_projections]
-
-    del all_logits, all_point_inds
-    torch.cuda.empty_cache()
-
-    cm.update(val_points_preds, val_points_labels)
-    miou, macc, oa, ious, accs = cm.all_metrics()
-    return miou, macc, oa, ious, accs
-
-
-@torch.no_grad()
-def test_entire_room(model, area, cfg, global_cm=None, num_votes=1):
-    """using a part of original point cloud as input to save memory.
-    Args:
-        model (_type_): _description_
-        test_loader (_type_): _description_
-        cfg (_type_): _description_
-        global_cm (_type_, optional): _description_. Defaults to None.
-        num_votes (int, optional): _description_. Defaults to 1.
-    Returns:
-        _type_: _description_
-    """
-    model.eval()  # set model to eval mode
-    cm = ConfusionMatrix(num_classes=cfg.num_classes)
-    global_cm =  ConfusionMatrix(num_classes=cfg.num_classes) if global_cm is None else global_cm
-    set_random_seed(0)
-    cfg.visualize = cfg.get('visualize', False)
-    if cfg.visualize:
-        from openpoints.dataset.vis3d import write_obj
-        cfg.vis_dir = os.path.join(cfg.run_dir, 'visualization')
-        os.makedirs(cfg.vis_dir, exist_ok=True)
-        cfg.cmap = cfg.cmap.astype(np.float32) / 255.
-        
-    # data
-    trans_split = 'val' if cfg.datatransforms.get('test', None) is None else 'test'
-    transform =  build_transforms_from_cfg(trans_split, cfg.datatransforms)
-
-    raw_root = os.path.join(cfg.dataset.common.data_root, 'raw')
-    data_list = sorted(os.listdir(raw_root))
-    data_list = [item[:-4] for item in data_list if 'Area_' in item]
-    data_list = [item for item in data_list if 'Area_{}'.format(area) in item]
-
-    voxel_size =  cfg.dataset.common.voxel_size
-    for cloud_idx, item in enumerate(tqdm(data_list)):
-        data_path = os.path.join(raw_root, item + '.npy')
-        cdata = np.load(data_path).astype(np.float32)  # xyz, rgb, label, N*7
-        coord_min = np.min(cdata[:, :3], 0)
-        cdata[:, :3] -= coord_min
-        label = torch.from_numpy(cdata[:, 6].astype(np.int).squeeze()).cuda(non_blocking=True)
-        colors = np.clip(cdata[:, 3:6] / 255., 0, 1).astype(np.float32)
-
-        all_logits, all_point_inds = [], []
-        if voxel_size is not None:
-            uniq_idx, count = voxelize(cdata[:, :3], voxel_size, mode=1)
-            for i in range(count.max()):
-                idx_select = np.cumsum(
-                    np.insert(count, 0, 0)[0:-1]) + i % count
-                idx_part = uniq_idx[idx_select]
-                np.random.shuffle(idx_part)
-                all_point_inds.append(idx_part)
-                coord, feat = cdata[idx_part][:,0:3] - np.min(cdata[idx_part][:, :3], 0), cdata[idx_part][:, 3:6]
-
-                data = {'pos': coord, 'x': feat}
-                if transform is not None:
-                    data = transform(data)
-                if 'heights' in data.keys():
-                    data['x'] = torch.cat((data['x'], data['heights']), dim=1)
-                else:
-                    data['x'] = torch.cat((data['x'], torch.from_numpy(
-                        coord[:, 3-cfg.dataset.common.get('n_shifted', 1):3].astype(np.float32))), dim=-1)
-
-                if not cfg.dataset.common.get('variable', False):
-                    data['x'] = data['x'].transpose(1, 0).unsqueeze(0)
-                    data['pos'] = data['pos'].unsqueeze(0)
-                else:
-                    data['o'] = torch.IntTensor([len(coord)])
-
-                keys = data.keys() if callable(data.keys) else data.keys
-                for key in keys:
-                    data[key] = data[key].cuda(non_blocking=True)
-                data['x'] = get_scene_seg_features(cfg.model.encoder_args.in_channels, data['pos'], data['x'])
-
-                logits = model(data)
-                all_logits.append(logits)
-                """visualization in debug mode 
-                from openpoints.dataset.vis3d import vis_points, vis_multi_points
-                # vis_points(cdata[:, :3], cdata[:, 3:6]/255.)
-                # vis_multi_points([cdata[:, :3], coord], [cdata[:, 3:6].astype(np.uint8), feat.astype(np.uint8)])
-                vis_multi_points([cdata[:, :3], coord], labels=[label.cpu().numpy(), logits.argmax(dim=1).squeeze().cpu().numpy()])
-                """
-            all_logits = torch.cat(all_logits, dim=0)
-            if not cfg.dataset.common.get('variable', False):
-                all_logits = all_logits.transpose(1, 2).reshape(-1, cfg.num_classes)
-            all_point_inds = torch.from_numpy(np.hstack(all_point_inds)).cuda(non_blocking=True)
-
-            if cfg.distributed:
-                dist.all_reduce(all_logits), dist.all_reduce(all_point_inds)
-
-            # project voxel subsampled to original set
-            all_logits = scatter(all_logits, all_point_inds, dim=0, reduce='mean')
-            all_point_inds = scatter(all_point_inds, all_point_inds, dim=0, reduce='mean')
-
-            cm.update(all_logits.argmax(dim=1), label)
-            global_cm.update(all_logits.argmax(dim=1), label)
-            
-            if cfg.visualize:
-                gt = label.cpu().numpy().squeeze()
-                pred = all_logits.argmax(dim=1).cpu().numpy().squeeze()
-                gt = cfg.cmap[gt, :]
-                pred = cfg.cmap[pred, :]
-                # output pred labels
-                write_obj(cdata[:, :3], colors,
-                          os.path.join(cfg.vis_dir, f'input-Area{area}-{cloud_idx}.obj'))
-                # output ground truth labels
-                write_obj(cdata[:, :3], gt,
-                          os.path.join(cfg.vis_dir, f'gt-Area{area}-{cloud_idx}.obj'))
-                # output pred labels
-                write_obj(cdata[:, :3], pred,
-                          os.path.join(cfg.vis_dir, f'pred-Area{area}-{cloud_idx}.obj'))
     tp, union, count = cm.tp, cm.union, cm.count
     if cfg.distributed:
         dist.all_reduce(tp), dist.all_reduce(union), dist.all_reduce(count)
     miou, macc, oa, ious, accs = get_mious(tp, union, count)
-    return miou, macc, oa, ious, accs, global_cm
+    return miou, macc, oa, ious, accs
 
 
 if __name__ == "__main__":
